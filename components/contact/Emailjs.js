92e0b9bdf72b2fc16069b1d62e06bc8b2bd4724e
Joakim Andersson <joakim.aandersson@live.se>
2021-04-12 15:23:46 +0200

    deploy debug, syntax

--- components/contact/Emailjs.js
+++ components/contact/Emailjs.js
@@ -2,18 +2,6 @@ import React from "react";
 import classes from "./emailjs.module.css";
 
 function Emailjs() {
-  function ValidateEmail(mail) {
-    if (
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-        myForm.emailAddr.value
-      )
-    ) {
-      return true;
-    }
-    alert("You have entered an invalid email address!");
-    return false;
-  }
-
   return (
     <div className={classes.formWrapper}>
       <div className={classes.title}>
@@ -21,19 +9,19 @@ function Emailjs() {
       </div>
       <form className={classes.form}>
         <input id="contact_number" type="hidden" name="contact_number" />
-        <label for="contact_number">
+        <label htmlFor="contact_number">
           <p>Namn</p>
         </label>
         <input id="user_name" type="text" name="user_name" />
-        <label for="user_name">
+        <label htmlFor="user_name">
           <p>E-post</p>
         </label>
         <input id="user_phone" type="text" name="user_phone" />
-        <label for="user_phone">
+        <label htmlFor="user_phone">
           <p>Telefon</p>
         </label>
         <input id="user_email" type="email" name="user_email" />
-        <label for="user_email">
+        <label htmlFor="user_email">
           <p>Meddelande</p>
         </label>
         <textarea name="message" />
@@ -43,19 +31,3 @@ function Emailjs() {
   );
 }
 export default Emailjs;
-
-/*function sendEmail(e) {
-    /*import emailjs from "emailjs-com";
-    e.preventDefault();
-
-    emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  }*/
