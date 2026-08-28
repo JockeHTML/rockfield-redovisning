4b0389dd219f3fe1f862b581ffafb956ef7c5797
Joakim Andersson <59662708+JockeHTML@users.noreply.github.com>
2026-08-28 15:05:13 +0200

    Update Map.js

--- components/contact/Map.js
+++ components/contact/Map.js
@@ -7,7 +7,8 @@ const Map = () => {
       <iframe
         className={classes.iframe}
         title="Rockfield Redovisning"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1847.6296551520097!2d17.223355616251943!3d62.413568067031235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46645dd3a3871afd%3A0xb6f08c182af27585!2sPonnyv%C3%A4gen%206%2C%20857%2050%20Sundsvall!5e0!3m2!1ssv!2sse!4v1617977125765!5m2!1ssv!2sse"
+        //src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1847.6296551520097!2d17.223355616251943!3d62.413568067031235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46645dd3a3871afd%3A0xb6f08c182af27585!2sPonnyv%C3%A4gen%206%2C%20857%2050%20Sundsvall!5e0!3m2!1ssv!2sse!4v1617977125765!5m2!1ssv!2sse"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1849.065472057674!2d17.302037777964955!3d62.39030176235833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4664675c32984373%3A0x3b9465a4d00f436f!2sThulegatan%2011%2C%20852%2032%20Sundsvall!5e0!3m2!1ssv!2sse!4v1787922167454!5m2!1ssv!2sse"
         width="100%"
         height="400"
         frameBorder="0"
